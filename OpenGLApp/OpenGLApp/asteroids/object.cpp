6d8b66ee99cbb7484a2c8db6588d439f84484fbe
FrancescoRisso <francesco@frisso.net>
2024-01-07 22:56:17 +0100

    Proportions are now screen-size-independent

--- OpenGLApp/OpenGLApp/asteroids/object.cpp
+++ OpenGLApp/OpenGLApp/asteroids/object.cpp
@@ -2,11 +2,24 @@
 
 using namespace Asteroids;
 
-Object::Object() : shader() {}
+extern unsigned int SCR_WIDTH;
+extern unsigned int SCR_HEIGHT;
+
+Object::Object() : shader(), pos(0), speed(0), transform(0) {}
 
 void Object::updateTransform() {
 	transform = glm::mat4((float) 1);
-	transform = glm::translate(transform, glm::vec3(pos, 0));
+
+	glm::vec2 scale(1);
+
+	if(SCR_WIDTH > SCR_HEIGHT)
+		scale.x = (float) SCR_HEIGHT / (float) SCR_WIDTH;
+	else
+		scale.y = (float) SCR_WIDTH / (float) SCR_HEIGHT;
+
+	transform = glm::scale(transform, glm::vec3(scale, 1));
+
+	transform = glm::translate(transform, glm::vec3(pos / scale, 0));
 	transform = glm::rotate(transform, angle, glm::vec3(0, 0, 1));
 }
 
