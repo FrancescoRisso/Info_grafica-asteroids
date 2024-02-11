70d1f710c97d432e249847dd74a7fec3d28a9b47
FrancescoRisso <francesco@frisso.net>
2024-02-11 14:40:58 +0100

    Simplified keyboard handler
    - removed duplicate WASD/arrow control: only arrows are now implemented
    - "debouncing" is no more time-dependent but press-dependent"
    - switched manual setting of state to automatic computation, to reduce code redundancy
    - removed useless renders
    - switched full re-initialization to string change

--- OpenGLApp/OpenGLApp/asteroids/game_logic/homePage.cpp
+++ OpenGLApp/OpenGLApp/asteroids/game_logic/homePage.cpp
@@ -21,6 +21,10 @@ void pausasec() {
 using namespace Asteroids;
 
 
+enum homePageKeys { keyDown, keyUp, space, keys_NUM };
+bool pressed[keys_NUM] = {false};
+
+
 enum homepageStrings { spacevoid, explore, instruct, highscore, quit, home_NUM_STRINGS };
 
 DisplayString homeStrings[home_NUM_STRINGS];
@@ -69,158 +73,45 @@ void updateTransformHomePage() {
 }
 
 
+void updateColors() {
+	for(int i = 1; i < home_NUM_STRINGS; i++) homeStrings[i].setColor(glm::vec3(state == i - 1 ? 1 : 0.5));
+}
+
+
 void processKeyboardHomePage(GLFWwindow* window) {
-	switch(state) {
-		case 0:
-		if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-
-			state = 1;
-			prepareHomePage();
-			//while(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) { pausasec();}
-			renderHomePage();
-			pausasec();
-		}
-		if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-
-			state = 1;
-			prepareHomePage();
-			//while(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) { pausasec(); }
-			renderHomePage();
-			pausasec();
-		}
-		if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-
-			state = 3;
-			prepareHomePage();
-			//while(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) { pausasec(); }
-			renderHomePage();
-			pausasec();
-		}
-		if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-
-			state = 3;
-			prepareHomePage();
-			//while(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) { pausasec(); }
-			renderHomePage();
-			pausasec();
-		}
-		if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-			prepareGame();
-			currentPhase = game;
-		}
-		break;
-		case 1:
-		if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-
-			state = 2;
-			prepareHomePage();
-			//while(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) { pausasec(); }
-			renderHomePage();
-			pausasec();
-		}
-		if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-
-			state = 2;
-			prepareHomePage();
-			//while(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) { pausasec(); }
-			renderHomePage();
-			pausasec();
-		}
-		if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-
-			state = 0;
-			prepareHomePage();
-			//while(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) { pausasec(); }
-			renderHomePage();
-			pausasec();
-		}
-		if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-
-			state = 0;
-			prepareHomePage();
-			//while(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) { pausasec(); }
-			renderHomePage();
-			pausasec();
-		}
-		if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-			prepareInstructions();
-			currentPhase = instructions; }
-		break;
-		case 2:
-		if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-
-			state = 3;
-			prepareHomePage();
-			/*while(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) { pausasec(); }*/
-			renderHomePage();
-			pausasec();
-		}
-		if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-
-			state = 3;
-			prepareHomePage();
-			/*while(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) { pausasec(); }*/
-			renderHomePage();
-			pausasec();
-		}
-		if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-
-			state = 1;
-			prepareHomePage();
-			/*while(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) { pausasec(); }*/
-			renderHomePage();
-			pausasec();
-		}
-		if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-
-			state = 1;
-			prepareHomePage();
-			/*while(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) { pausasec(); }*/
-			renderHomePage();
-			pausasec();
-		}
-		if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-			resetHighScore();
-			//prepareHS();
-			//renderHS();
-		}
-		break;
-		default: 
-			if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-
-			state = 0;
-			prepareHomePage();
-			/*while(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) { pausasec(); }*/
-			renderHomePage();
-			pausasec();
-		}
-			if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-
-			state = 0;
-			prepareHomePage();
-			/*while(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) { pausasec(); }*/
-			renderHomePage();
-			pausasec();
-		}
-			if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-
-			state = 2;
-			prepareHomePage();
-			/*while(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) { pausasec(); }*/
-			renderHomePage();
-			pausasec();
-		}
-			if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-
-			state = 2;
-			prepareHomePage();
-			//while(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) { pausasec(); }
-			renderHomePage();
-			pausasec();
-		}
-			if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) { glfwSetWindowShouldClose(window, true); }
-			break;
+	if(!pressed[keyDown] && glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+		state = (state + 1) % 4;
+		updateColors();
+		pressed[keyDown] = true;
 	}
+
+	if(!pressed[keyUp] && glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+		state = (state - 1) % 4;
+		updateColors();
+		pressed[keyUp] = true;
+	}
+
+	if(!pressed[space] && glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+		switch(state) {
+			case 0:
+				prepareGame();
+				currentPhase = game;
+				break;
+			case 1:
+				prepareInstructions();
+				currentPhase = instructions;
+				break;
+			case 2:
+				resetHighScore();
+				homeStrings[highscore].changeString("Best score: 0");
+				break;
+			case 3: glfwSetWindowShouldClose(window, true); break;
+		}
+	}
+
+	if(pressed[keyDown] && glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_RELEASE) pressed[keyDown] = false;
+	if(pressed[keyUp] && glfwGetKey(window, GLFW_KEY_UP) == GLFW_RELEASE) pressed[keyUp] = false;
+	if(pressed[space] && glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE) pressed[space] = false;
 }
 
 
