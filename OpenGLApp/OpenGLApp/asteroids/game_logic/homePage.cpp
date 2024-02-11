e455b42e730b13e29194d7ac45ac582807176dcc
FrancescoRisso <francesco@frisso.net>
2024-02-11 14:36:14 +0100

    Simplified preparation function
    Several redundant initialization replaced with some ternary operators

--- OpenGLApp/OpenGLApp/asteroids/game_logic/homePage.cpp
+++ OpenGLApp/OpenGLApp/asteroids/game_logic/homePage.cpp
@@ -11,7 +11,7 @@ void pausasec() {
 	// Calcola l'istante di tempo futura (mezzo secondo dopo l'istante corrente)
 	auto istanteFutura = oraCorrente + std::chrono::milliseconds(300);
 
-	// Attendi finché l'istante di tempo futura non viene raggiunto
+	// Attendi finchï¿½ l'istante di tempo futura non viene raggiunto
 	while(std::chrono::high_resolution_clock::now() < istanteFutura) {
 		// Attendi...
 	}
@@ -43,41 +43,12 @@ void prepareHomePage() {
 	int highScore = getHighScore();
 	if(highScore == -1) highScore++;
 	sprintf_s(buf, "Best score: %d", highScore);
-	switch(state) {
-		case 0: 
-		homeStrings[spacevoid].Init(glm::vec2(0, 0.65), " SPACEVOID", alignCenterHoriz, alignCenterVert, glm::vec3(1), 0.25);
-		homeStrings[explore].Init(glm::vec2(0, 0.15), "Start Exploring", alignCenterHoriz, alignCenterVert, glm::vec3(1), 0.10);
-		homeStrings[instruct].Init(glm::vec2(0, 0.0), "Instructions", alignCenterHoriz, alignCenterVert, glm::vec3(0.5), 0.10);
-		homeStrings[highscore].Init(glm::vec2(0, -0.15), buf, alignCenterHoriz, alignCenterVert, glm::vec3(0.5), 0.10);
-		homeStrings[quit].Init(glm::vec2(0, -0.30), "Quit", alignCenterHoriz, alignCenterVert, glm::vec3(0.5), 0.10);
-		break;
-		case 1:
-		homeStrings[spacevoid].Init(glm::vec2(0, 0.65), " SPACEVOID", alignCenterHoriz, alignCenterVert, glm::vec3(1), 0.25);
-		homeStrings[explore].Init(glm::vec2(0, 0.15), "Start Exploring", alignCenterHoriz, alignCenterVert, glm::vec3(0.5), 0.10);
-		homeStrings[instruct].Init(glm::vec2(0, 0.0), "Instructions", alignCenterHoriz, alignCenterVert, glm::vec3(1), 0.10);
-		homeStrings[highscore].Init(glm::vec2(0, -0.15), buf, alignCenterHoriz, alignCenterVert, glm::vec3(0.5), 0.10);
-		homeStrings[quit].Init(glm::vec2(0, -0.30), "Quit", alignCenterHoriz, alignCenterVert, glm::vec3(0.5), 0.10);
-		break;
-		case 2:
-		homeStrings[spacevoid].Init(glm::vec2(0, 0.65), " SPACEVOID", alignCenterHoriz, alignCenterVert, glm::vec3(1), 0.25);
-		homeStrings[explore].Init(glm::vec2(0, 0.15), "Start Exploring", alignCenterHoriz, alignCenterVert, glm::vec3(0.5), 0.10);
-		homeStrings[instruct].Init(glm::vec2(0, 0.0), "Instructions", alignCenterHoriz, alignCenterVert, glm::vec3(0.5), 0.10);
-		homeStrings[highscore].Init(glm::vec2(0, -0.15), "Reset Highscores", alignCenterHoriz, alignCenterVert, glm::vec3(1), 0.10);
-		homeStrings[quit].Init(glm::vec2(0, -0.30), "Quit", alignCenterHoriz, alignCenterVert, glm::vec3(0.5), 0.10);
-		break;
-		default:
-		homeStrings[spacevoid].Init(glm::vec2(0, 0.65), " SPACEVOID", alignCenterHoriz, alignCenterVert, glm::vec3(1), 0.25);
-		homeStrings[explore].Init(glm::vec2(0, 0.15), "Start Exploring", alignCenterHoriz, alignCenterVert, glm::vec3(0.5), 0.10);
-		homeStrings[instruct].Init(glm::vec2(0, 0.0), "Instructions", alignCenterHoriz, alignCenterVert, glm::vec3(0.5), 0.10);
-		homeStrings[highscore].Init(glm::vec2(0, -0.15), buf, alignCenterHoriz, alignCenterVert, glm::vec3(0.5), 0.10);
-		homeStrings[quit].Init(glm::vec2(0, -0.30), "Quit", alignCenterHoriz, alignCenterVert, glm::vec3(1), 0.10);
-		break;
-	}
-	/* instrStrings[shoot].Init(glm::vec2(-0.90, -0.3), " - shoot with SPACE", alignLeft, alignCenterVert, glm::vec3(1), 0.10);
-	instrStrings[move].Init(glm::vec2(-0.90, -0.45), " - move with WASD", alignLeft, alignCenterVert, glm::vec3(1), 0.10);
-	instrStrings[lives].Init(glm::vec2(-0.90, -0.6), "Once you finish your 3 lives, the game ends.", alignLeft, alignCenterVert, glm::vec3(1), 0.10);
-	instrStrings[returnMain].Init(glm::vec2(0.90, -0.8), "Press M to return to main menu.", alignRight, alignCenterVert, glm::vec3(1), 0.06);
-*/
+
+	homeStrings[spacevoid].Init(glm::vec2(0, 0.65), " SPACEVOID", alignCenterHoriz, alignCenterVert, glm::vec3(1), 0.25);
+	homeStrings[explore].Init(glm::vec2(0, 0.15), "Start Exploring", alignCenterHoriz, alignCenterVert, glm::vec3(state == 0 ? 1 : 0.5), 0.10);
+	homeStrings[instruct].Init(glm::vec2(0, 0.0), "Instructions", alignCenterHoriz, alignCenterVert, glm::vec3(state == 1 ? 1 : 0.5), 0.10);
+	homeStrings[highscore].Init(glm::vec2(0, -0.15), buf, alignCenterHoriz, alignCenterVert, glm::vec3(state == 2 ? 1 : 0.5), 0.10);
+	homeStrings[quit].Init(glm::vec2(0, -0.30), "Quit", alignCenterHoriz, alignCenterVert, glm::vec3(state == 3 ? 1 : 0.5), 0.10);
 }
 
 void logoPrint() {
